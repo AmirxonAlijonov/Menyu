95d75f47f9de0a25f10ee24dfca8ad7ca207bb43
Amirxon <alijonovamirxon007@gmail.com>
2026-03-05 13:15:20 +0500

    mahsulotlar to'g'rilandi

--- index.js
+++ index.js
@@ -12,20 +12,14 @@ const foodData = {
         {
             title: "Fransuz Salati",
             description: "Yangi sabzavotlar, pyuresi, tuxum va maxsus french sousi bilan.",
-            price: "50,000 so'm",
+            price: "35,000 so'm",
             image: "https://images.unsplash.com/photo-1546793665-c74683f339c1?w=800"
         },
         {
             title: "Sezer Salati",
             description: "Romsalat, parmesan, croutons va caesar sousi bilan.",
-            price: "55,000 so'm",
+            price: "35,000 so'm",
             image: "https://images.unsplash.com/photo-1550304943-4f24f54ddde9?w=800"
-        },
-        {
-            title: "Kapus Salati",
-            description: "Yangi karam, sabzi va maxsus sous bilan.",
-            price: "38,000 so'm",
-            image: "https://images.unsplash.com/photo-1540420773420-3366772f4999?w=800"
         },
         {
             title: "Svejiy Salat",
@@ -36,108 +30,132 @@ const foodData = {
         {
             title: "Salyoni Salat",
             description: "Svezhiy losos, mix-salot, avokado va sitrus sousi bilan.",
-            price: "65,000 so'm",
+            price: "35,000 so'm",
             image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=800"
         }
     ],
     mains: [
         {
-            title: "Kolbasali Pasta",
-            description: "Italyancha pasta, maxsus sous va mazali kolbasa bilan. Pasta - Italiyaning milliy taomi hisoblanadi. Uning ko'plab turlari mavjud: spaghetti, penne, fettuccine va boshqalar. Kolbasa esa taomga ajoyib ta'm beradi.",
-            price: "55,000 so'm",
-            image: "https://images.unsplash.com/photo-1551183053-bf91a1d81141?w=800"
+            title: "Jiz",
+            description: "Mol go'shtidan tayyorlangan mazali taom. Qiyma go'sht, piyoz va maxsus ziravorlar bilan.",
+            price: "220,000 so'm",
+            image: "https://images.unsplash.com/photo-1529692236671-f1f6cf9683ba?w=800"
         },
         {
-            title: "Margarita Pitsasi",
-            description: "Italyancha uslubda tayyorlangan, makkajo'xori va motsarella pishlog'i bilan. Bu pitsa italiyalik oshpaz Raffaele Esposito tomonidan 1889-yilda Neapolda ixtiro qilingan. Ingredientlar: pomidor sousi, motsarella, bazilik.",
-            price: "75,000 so'm",
-            image: "https://images.unsplash.com/photo-1513104890138-7c749659a591?w=800"
+            title: "Tabaka",
+            description: "Butun tovuqni yog'da press bilan bosib tayyorlangan shirali va mazali taom.",
+            price: "60,000 so'm",
+            image: "https://images.unsplash.com/photo-1598103442097-8b74394b95c6?w=800"
         },
         {
-            title: "Classic Burger",
-            description: "Juicy go'sht, yangi sabzavotlar va maxsus sous bilan. Burger - Amerikada ixtiro qilingan va dunyo bo'ylab juda mashhur fast food taomi. Buni tayyorlash uchun yaxshi sifatli go'sht va yangi ingredientlar kerak.",
-            price: "65,000 so'm",
-            image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=800"
-        },
-        {
-            title: "Asalli Pankeyk",
-            description: "Yumshoq pankeyklar, yangi rezavor mevalar va tabiiy asal bilan. Pankeyk - amerika oshxonasining mashhur taomidir. U tuxum, un, sut va xamirturishdan tayyorlanadi va asal yoki jem bilan dasturxonga tortiladi.",
-            price: "35,000 so'm",
-            image: "https://images.unsplash.com/photo-1506084868230-bb9d95c24759?w=800"
-        },
-        {
-            title: "Milliy Osh",
-            description: "O'zbek osh - milliy taomimiz. Jo'xori, qo'y go'shti, piyoz, морковь va maxsus ziravorlar bilan tayyorlanadi. Bu taom O'zbekistonda har bir bayram stolining bosh go'zalligi hisoblanadi.",
+            title: "KFS",
+            description: "Maxsus marinadlangan qovurilgan tovuq va qovurilgan kartoshka (fri) bilan. KFS - mashhur fast food taomi.",
             price: "80,000 so'm",
-            image: "https://images.unsplash.com/photo-1563379926898-05f4575a45d8?w=800"
+            image: "https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?w=800"
+        },
+        {
+            title: "Barbekyu",
+            description: "Go'shtni maxsus barbekyu sousi bilan grillda pishirilgan mazali taom.",
+            price: "220,000 so'm",
+            image: "https://images.unsplash.com/photo-1529193591184-b1d58069ecdd?w=800"
+        },
+        {
+            title: "Baliq",
+            description: "Yangi baliqni yog'da qovurilgan holda tayyorlangan mazali taom. KFS uslubida yog'da qovurilgan.",
+            price: "180,000 so'm",
+            image: "https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?w=800"
         },
         {
             title: "Mol Go'shti Shashlik",
             description: "Maxsus marinadlangan mol go'shtidan tayyorlangan shirali shashlik. Uzun vaqt davomida kokilarda pishiriladi va ajoyib ta'mga ega bo'ladi.",
-            price: "70,000 so'm",
+            price: "110,000 so'm",
             image: "https://images.unsplash.com/photo-1603360946369-dc9bb6258143?w=800"
         },
         {
-            title: "Lavash",
-            description: "Yangi non, go'sht, pomidor, bodring va maxsus sous bilan tayyorlangan mazali taom. Tez vaqt ichida tayyorlanadi va juda mazali.",
-            price: "50,000 so'm",
-            image: "https://images.unsplash.com/photo-1599974579688-8dbdd335c77f?w=800"
+            title: "Manti",
+            description: "Go'sht va sabzavotlar bilan tayyorlangan an'anaviy O'zbek taomi. Bug'da pishiriladi.",
+            price: "7,000 so'm",
+            image: "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?w=800"
         },
         {
-            title: "O'zbek Sho'rva",
-            description: "Guruch, go'sht va sabzavotlar bilan tayyorlangan mazali sho'rva. Bu taom sovuq kunning eng yaxshi tanlovi hisoblanadi.",
-            price: "45,000 so'm",
+            title: "Tandir Somsa",
+            description: "Tandirda pishirilgan go'shtli an'anaviy somsa.",
+            price: "15,000 so'm",
+            image: "https://images.unsplash.com/photo-1601050690597-df0568f70950?w=800"
+        },
+        {
+            title: "Mastava",
+            description: "Guruch, go'sht va sabzavotlar bilan tayyorlangan an'anaviy O'zbek sho'rvasi.",
+            price: "35,000 so'm",
             image: "https://images.unsplash.com/photo-1547592166-23ac45744acd?w=800"
         },
         {
-            title: "Bifshteks",
-            description: "Yuqori sifatli mol go'shtidan tayyorlangan nazik steak. Maxsus ziravorlar bilan marinadlangan va tamamli pishirilgan.",
-            price: "90,000 so'm",
+            title: "Qaynatma Sho'rva",
+            description: "Go'shtni uzoq vaqt davomida qaynatib tayyorlangan to'qimali sho'rva.",
+            price: "35,000 so'm",
             image: "https://images.unsplash.com/photo-1544025162-d76694265947?w=800"
+        },
+        {
+            title: "Grechka",
+            description: "Grechka yoki sovuq - go'sht va sabzavotlar bilan tayyorlangan mazali taom.",
+            price: "35,000 so'm",
+            image: "https://images.unsplash.com/photo-1547592166-23ac45744acd?w=800"
+        },
+        {
+            title: "Ko'za Sho'rva",
+            description: "Ko'zada tayyorlangan go'sht va sabzavotli an'anaviy sho'rva.",
+            price: "80,000 so'm",
+            image: "https://images.unsplash.com/photo-1547592166-23ac45744acd?w=800"
         }
     ],
     drinks: [
         {
             title: "Coca Cola",
-            description: "Gazli ichimlik 0.5l hajmda. Sovuq va tetiklashtiruvchi ichimlik.",
-            price: "15,000 so'm",
+            description: "Gazli ichimlik 1.5l hajmda. Sovuq va tetiklashtiruvchi ichimlik.",
+            price: "17,000 so'm",
             image: "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=800"
         },
         {
             title: "Yashil Choy",
             description: "Issiq yashil choy limon va asal bilan. Bu ichimlik sog'liq uchun juda foydali va tetiklashtiradi.",
-            price: "10,000 so'm",
+            price: "2,000 so'm",
             image: "https://images.unsplash.com/photo-1556679343-c7306c1976bc?w=800"
         },
         {
             title: "Fanta",
-            description: "Gazli apelsinli ichimlik 0.5l hajmda. Mashhur va tetiklashtiruvchi ichimlik.",
-            price: "15,000 so'm",
+            description: "Gazli apelsinli ichimlik 1.5l hajmda. Mashhur va tetiklashtiruvchi ichimlik.",
+            price: "17,000 so'm",
             image: "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=800"
         },
         {
             title: "Pepsi",
-            description: "Gazli ichimlik 0.5l hajmda. Mashhur va tetiklashtiruvchi ichimlik.",
-            price: "15,000 so'm",
+            description: "Gazli ichimlik 1.5l hajmda. Mashhur va tetiklashtiruvchi ichimlik.",
+            price: "17,000 so'm",
             image: "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=800"
         },
         {
             title: "Qora Choy",
             description: "Issiq qora choy suty bilan. An'anaviy ichimlik.",
-            price: "12,000 so'm",
+            price: "5,000 so'm",
             image: "https://images.unsplash.com/photo-1556679343-c7306c1976bc?w=800"
         },
         {
-            title: "Latte Kofe",
-            description: "Issiq latte kofe sut va qahva bilan. Kofe ixlasmandlari uchun eng yaxshi tanlov.",
-            price: "30,000 so'm",
-            image: "https://images.unsplash.com/photo-1572442388796-11668a67e53d?w=800"
+            title: "Limon Choy",
+            description: "Maxsus tayyorlangan limonli choy - yangi limon va choy bilan tayyorlangan.",
+            price: "20,000 so'm",
+            image: "https://images.unsplash.com/photo-1556679343-c7306c1976bc?w=800"
         },
         {
-            title: "Limон Moxito",
-            description: "Yangicha limon, мятa va gazli suv bilan tayyorlangan tetiklashtiruvchi ichimlik.",
-            price: "35,000 so'm",
-            image: "https://images.unsplash.com/photo-1551538827-9c037cb4f32a?w=800"
+            title: "Sok",
+            description: "Tabiiy meva sharbati - aralash mevalar.",
+            price: "30,000 so'm",
+            image: "https://images.unsplash.com/photo-1621506289937-a8e4df240d0b?w=800"
+        },
+        {
+            title: "Sprite",
+            description: "Gazli limonato ichimlik 1.5l hajmda. Sovuq va tetiklashtiruvchi ichimlik.",
+            price: "17,000 so'm",
+            image: "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=800"
         }
     ]
 };
@@ -158,10 +176,10 @@ function openCategory(category) {
     currentIndexCategory = 0;
     
     // Slaydni ko'rsatish
-    showSlideCategory(0);
+    // showSlideCategory(0);
     
     // Avtomatik aylanishni boshlash
-    startAutoSlideCategory();
+    // startAutoSlideCategory();
 }
 
 // Bosh sahifani ko'rsatish
@@ -175,7 +193,7 @@ function showMainPage() {
     document.getElementById('mainPage').classList.add('active');
     
     // Avtomatik aylanishni to'xtatish
-    clearInterval(autoSlide);
+    // clearInterval(autoSlide);
 }
 
 // Slaydni ko'rsatish funksiyasi (category uchun)
@@ -272,7 +290,7 @@ function closeFullscreen() {
     document.body.style.overflow = '';
 
     // Avtomatik aylanishni qayta ishga tushirish
-    startAutoSlideCategory();
+    // startAutoSlideCategory();
 }
 
 // Buyurtma berish funksiyasi
@@ -289,17 +307,17 @@ document.addEventListener('keydown', (e) => {
         }
     } else {
         if (e.key === 'ArrowLeft') {
-            // Oldingi slayd
-            const items = foodData[currentCategory];
-            currentIndexCategory = (currentIndexCategory - 1 + items.length) % items.length;
-            showSlideCategory(currentIndexCategory);
-            resetAutoSlideCategory();
+            // Oldingi slayd - OLD
+            // const items = foodData[currentCategory];
+            // currentIndexCategory = (currentIndexCategory - 1 + items.length) % items.length;
+            // showSlideCategory(currentIndexCategory);
+            // resetAutoSlideCategory();
         } else if (e.key === 'ArrowRight') {
-            // Keyingi slayd
-            const items = foodData[currentCategory];
-            currentIndexCategory = (currentIndexCategory + 1) % items.length;
-            showSlideCategory(currentIndexCategory);
-            resetAutoSlideCategory();
+            // Keyingi slayd - OLD
+            // const items = foodData[currentCategory];
+            // currentIndexCategory = (currentIndexCategory + 1) % items.length;
+            // showSlideCategory(currentIndexCategory);
+            // resetAutoSlideCategory();
         } else if (e.key === 'Enter') {
             openFullscreen(currentCategory);
         }
